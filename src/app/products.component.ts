f95af1d36429b4b3744a55c21337940ce5c22971
Kevin Giorgio Toledo De La cruz <kgtoledoc19@gmail.com>
2019-03-16 05:45:19 -0500

    Using directives like ngFor and ngIf

--- src/app/products.component.ts
+++ src/app/products.component.ts
@@ -8,12 +8,17 @@ import { Component } from '@angular/core';
 export class ProductsComponent {
   productName = "A Book";
   isDisable = true;
-
+  products = ["A book", 'A tree'];
   constructor() {
     setTimeout(() => {
       //this.productName = "A Tree"
       this.isDisable = false;
     }, 3000)
+  }
+
+  onAddProduct() {
+    this.products.push(this.productName)
+
   }
 
 }
